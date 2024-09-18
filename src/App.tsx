641449f869c991b51eccf6614ee66876e91b5cf4
Connor van Spronssen <connor.van.spronssen@iodigital.com>
2024-09-18 08:19:37 +0200

    Add a performance overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Box from "./Objects/Box"
 import { Sky } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import { Perf } from "r3f-perf"
 import { Euler } from "three"
 
 function App() {
   return (
     <div id="canvas-container">
       <Canvas>
+        <Perf position="top-left" />
+
         <Sky sunPosition={[100, 20, 100]} />
         <ambientLight intensity={0.1} />
         <directionalLight position={[0, 0, 5]} />
